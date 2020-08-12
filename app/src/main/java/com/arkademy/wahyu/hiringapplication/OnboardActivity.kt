d5cac7acf430d89608fd77b6c942ea38d5914b8b
wahyukurniawaan <wahyukurniawaan@gmail.com>
2020-08-12 10:35:13 +0700

    menambahkan function lifecycle di OnboardActivity.kt

--- app/src/main/java/com/arkademy/wahyu/hiringapplication/OnboardActivity.kt
+++ app/src/main/java/com/arkademy/wahyu/hiringapplication/OnboardActivity.kt
@@ -2,9 +2,10 @@ package com.arkademy.wahyu.hiringapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
 class OnboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +26,52 @@ class OnboardActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        // menampilkan life cycle dari activity android
+        Toast.makeText(this, "activity: onCreate()", Toast.LENGTH_LONG).show()
+        Log.d("lifecycle", "onCreate()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Toast.makeText(this, "activity: onStart()", Toast.LENGTH_SHORT).show()
+        Log.d("lifecycle", "onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Toast.makeText(this, "activity: onResume()", Toast.LENGTH_SHORT).show()
+        Log.d("lifecycle", "onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Toast.makeText(this, "activity: onPause()", Toast.LENGTH_SHORT).show()
+        Log.d("lifecycle", "onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Toast.makeText(this, "activity: onStop()", Toast.LENGTH_SHORT).show()
+        Log.d("lifecycle", "onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Toast.makeText(this, "activity: onRestart()", Toast.LENGTH_SHORT).show()
+        Log.d("lifecycle", "onRestart()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Toast.makeText(this, "activity: onDestroy()", Toast.LENGTH_SHORT).show()
+        Log.d("lifecycle", "onDestroy()")
     }
 }
+
